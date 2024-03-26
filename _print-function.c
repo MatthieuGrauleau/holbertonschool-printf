460d3df278f7b82abe2d5ba842880d63d7e66447
Elmael33 <mael.salet33@gmail.com>
2024-03-26 14:39:43 +0100

    modification betty for _print-function

--- _print-function.c
+++ _print-function.c
@@ -4,6 +4,8 @@
 * print_c - print a character
 *
 * @args: arguments to prints
+*
+* Return: character
 */
 
 int print_c(va_list args)
@@ -12,8 +14,17 @@ int print_c(va_list args)
 
 	c = va_arg(args, int);
 	_putchar(c);
-	return(1);
+	_putchar('\n');
+	return (1);
 }
+
+/**
+* print_s - print a string
+*
+* @args: arguments to print
+*
+* Return: String
+*/
 
 int print_s(va_list args)
 {
@@ -25,10 +36,17 @@ int print_s(va_list args)
 
 	for (i = 0; i < len; i++)
 	{
-		_putchar(s[i]);
+		_putchar (s[i]);
 	}
-	return(1);
+	_putchar('\n');
+	return (1);
 }
+
+/**
+ * print_percentage -print a percentage
+ *
+ * Return: percentage
+*/
 
 int print_percentage(void)
 {
