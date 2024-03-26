a2c5ce0970fdc3efc91ae11c478b2388d3c3bb4a
MatthieuGrauleau <m.grauleau@gmail.com>
2024-03-26 09:26:56 +0100

    change test for character print

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	_printf("%c", 'H');
+	_printf("Character:[%c]\n", 'H');
 	printf("Character:[%c]\n", 'H');
 	_printf("String:[%s]\n", "I am a string !");
 	printf("String:[%s]\n", "I am a string !");
